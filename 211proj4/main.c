622965ba4445309878a63df524e324aca5861438
spacecoffin <reed.rosenberg@gmail.com>
2014-11-12 22:55:07 -0500

    working on ht_create
    reread the ht section from Lec 8.

--- 211proj4/main.c
+++ 211proj4/main.c
@@ -1,25 +1,25 @@
-//
-//  hashtable.c
-//  211proj4
-//
-//  Created by spacecoffin on 11/11/14.
-//  Copyright (c) 2014 spacecoffin. All rights reserved.
-//
+/*
+ * Reed Rosenberg
+ * CS 211 Assignment 4
+ */
 
-#include <stdio.h>
+#include <stdio.h>		// for IO functions
+#include <stdlib.h>		// for Dynamic Memory Allocation
 
-// Type definition for a node
+// Global variables & macro definitions
+#define HASH_MULTIPLIER 65599
+int htsize;			// size (in buckets) of hash table
+
+// Type definition for a Node
 struct NodeType {
 	char *word;		// a string representing the word
 	int count;		// the # of times that word appears in the text
 	struct NodeType *next;	// pointer to next node in list (if it exists)
 };
-typedef struct NodeType Node;
+typedef struct NodeType Node;	// define Node to be synonymous with NodeType
 
 // Function prototypes
 Node **ht_create(void);
-
-int htsize;			// size (in buckets) of hash table
 
 int main(int argc, char *argv[])
 {
@@ -27,6 +27,29 @@ int main(int argc, char *argv[])
 	if (argc <= 1) {
 		printf("ERROR: Usage: %s table_size\n", argv[0]);
 		return 1;
-	}
+	} // else if for extra arguments????
 	htsize = atoi(argv[1]);
+	
+	Node **Table;		// a pointer to the heap-allocated hash table
+	Table = ht_create();	// create hash table
+	
+	/*
+	 * If the table cannot be allocated, issue an error message on stderr 
+	 * and terminate program.
+	 */
+	if (Table == NULL) {
+		fprintf(stderr, "ERROR: malloc failed\n");
+		return 1;
+	}
+}
+
+/*
+ * Create a heap-allocated hash table with htsize buckets, initially empty, and
+ * return a pointer to it. If the table cannot be allocated, issue an error
+ * message on stderr and terminate program.
+ */
+Node **ht_create(void)
+{
+	Node **ht = (Node **) malloc(htsize * sizeof(Node *));
+	return ht;
 }
